6a043828cc304eee055c0d298000fcd40edfacd8
Shawn McCool <shawn@heybigname.com>
2014-06-08 07:58:47 +0200

    change project from phpunit to phpspec

--- src/Proof.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Bowling;
-
-class Proof
-{
-} 
--- tests/ProofTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ProofTest extends PHPUnit_Framework_TestCase {
-
-    public function testCans()
-    {
-        $this->assertInstanceOf('Bowling\Proof', new Bowling\Proof());
-    }
-}
